524d00b1cecfbcfd8101330ab1b11e6121f60aca
sakurakotubaki <gitonly543@gmail.com>
2025-01-16 22:11:33 +0900

    delete: unnecessary code

--- composeApp/src/commonMain/kotlin/org/jboycode/App.kt
+++ composeApp/src/commonMain/kotlin/org/jboycode/App.kt
@@ -2,9 +2,7 @@ package org.jboycode
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
